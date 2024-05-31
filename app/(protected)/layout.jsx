ab852ea6586f3e5422b4ed2981de4e505d886131
sdharrin <138178989+sdharrin@users.noreply.github.com>
2024-05-31 12:00:00 -0700

    sidebar adjusted

--- app/(protected)/layout.jsx
+++ app/(protected)/layout.jsx
@@ -7,19 +7,19 @@ import Sidebar from "@/components/ui/sidebar";
 const ProtectedLayout = ({ children }) => {
   return (
     <div>
-      <div className="w-full h-full flex">
-        <div className="hidden md:flex flex-1 w-full sticky top-0 p-5 bg-bgSoft">
+      <div className="flex w-full h-full">
+        <div className="sticky top-0 hidden p-5 md:flex bg-bgSoft">
           <Sidebar />
         </div>
-        <div className="flex-4 lg:flex-5 flex flex-col w-full">
-          <div className="flex justify-between w-full pt-5 pb-5 items-center bg-bgSoft">
+        <div className="flex flex-col w-full flex-4 lg:flex-5">
+          <div className="flex items-center justify-between w-full pt-5 pb-5 bg-bgSoft">
             <Navbar />
           </div>
-          <div>{children}</div>
+          <div className="m-4 bg-bgSoft">{children}</div>
         </div>
         <div className="hidden md:flex w-[20px] bg-bgSoft"></div>
       </div>
-      <div className="w-full bg-bgSoft h-20 hidden md:flex justify-between items-center">
+      <div className="items-center justify-between hidden w-full h-20 bg-bgSoft md:flex">
         <Footer />
       </div>
     </div>
--- components/ui/sidebar.jsx
+++ components/ui/sidebar.jsx
@@ -14,8 +14,19 @@ import { BsFillQuestionSquareFill } from "react-icons/bs";
 import { AiOutlineFileText } from "react-icons/ai";
 import Link from "next/link";
 import Footer from "./footer";
-import { MdSupervisedUserCircle } from "react-icons/md";
+import {
+  MdArrowCircleLeft,
+  MdArrowCircleRight,
+  MdSupervisedUserCircle,
+} from "react-icons/md";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./tooltip";
 
 const menuList = [
   {
@@ -91,44 +102,74 @@ const menuList = [
 ];
 
 const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
   const pathname = usePathname();
   const itme = pathname.split("/").pop();
-  console.log(itme);
 
   return (
-    <div className="sticky top-0 w-full">
+    <div className="sticky top-0 h-screen">
       <div className="flex items-center justify-center">
-        <div className="ml-10 mr-5 flex">
-          <MdSupervisedUserCircle size={60} />
+        <div className={isOpen ? "flex ml-10 mr-5" : ""}>
+          <MdSupervisedUserCircle size={isOpen ? 60 : 40} />
         </div>
-        <div className="flex flex-col w-full">
+        <div className={isOpen ? "flex flex-col" : "hidden"}>
           <span className="text-sm">Shawn Harrington</span>
           <span className="text-xs text-textSoft">Administrator</span>
         </div>
       </div>
-      <hr className="border mt-5" />
+      <hr className={`${isOpen ? "mt-5" : "mt-10"} border`} />
+      <div
+        className="flex items-center justify-center mt-5 cursor-pointer hover:bg-white hover:rounded-md hover:text-black"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className="text-lg">
+          <span>{isOpen ? <MdArrowCircleLeft /> : <MdArrowCircleRight />}</span>
+        </div>
+      </div>
       <ul>
         {menuList.map((menu, index) => {
           return (
             <li key={index} className="mt-10">
-              <div className="text-sm text-textSoft font-bold mb-2">
-                {menu.title}
+              <div
+                className={
+                  isOpen
+                    ? "mb-2 text-sm font-bold text-textSoft"
+                    : "mb-2 text-sm font-bold text-textSoft"
+                }
+              >
+                {isOpen ? menu.title : menu.title}
               </div>
-              <ul className="flex flex-col gap-4 ml-4">
+              <ul
+                className={`${
+                  isOpen ? "" : "items-center gap-4"
+                } flex flex-col gap-4 transition-all duration-300`}
+              >
                 {menu.list.map((item, index) => {
                   return (
                     <li key={index}>
-                      <Link
-                        href={item.path}
-                        className={`hover:bg-white hover:rounded-md hover:text-black flex gap-4 items-center px-2 py-1 w-full text-lg ${
-                          itme === item.title
-                            ? "bg-white rounded-md text-black"
-                            : ""
-                        }`}
-                      >
-                        <div className="">{item.icon}</div>
-                        <div className="capitalize">{item.title}</div>
-                      </Link>
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger className="w-full">
+                            <Link
+                              href={item.path}
+                              className={`hover:bg-white hover:rounded-md hover:text-black flex gap-4 items-center px-2 py-1 w-full text-lg ${
+                                itme === item.title
+                                  ? "bg-white rounded-md text-black"
+                                  : ""
+                              }`}
+                            >
+                              <div className="">{item.icon}</div>
+                              <div className={isOpen ? "capitalize" : "hidden"}>
+                                {item.title}
+                              </div>
+                            </Link>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p>{item.title}</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
                     </li>
                   );
                 })}
