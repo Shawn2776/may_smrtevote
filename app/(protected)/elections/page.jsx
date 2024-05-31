5cc3c07775923a6136798ca6099cd1eb90a5f0d8
sdharrin <138178989+sdharrin@users.noreply.github.com>
2024-05-31 14:11:02 -0700

    elections formatted

--- app/(protected)/elections/page.jsx
+++ app/(protected)/elections/page.jsx
@@ -1,7 +1,100 @@
 import React from "react";
+import { elections } from "@/data/";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Link from "next/link";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { MdDelete, MdMore, MdUpdate } from "react-icons/md";
 
 const ElectionsPage = () => {
-  return <div>ElectionsPage</div>;
+  return (
+    <div className="min-h-screen bg-bg">
+      <div className="p-5 rounded-md bg-bgSoft">
+        <Table>
+          <TableCaption>A list of your elections.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Title</TableHead>
+              <TableHead>Date Range</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Description</TableHead>
+              <TableHead>Candidates</TableHead>
+              <TableHead>% Complete</TableHead>
+              <TableHead>Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {elections.map((election) => (
+              <TableRow key={election.id}>
+                <TableCell className="font-medium">{election.title}</TableCell>
+                <TableCell>{`${election.startDate} - ${election.endDate}`}</TableCell>
+                <TableCell>
+                  <span
+                    className={`capitalize rounded-md px-2 py-1 ${
+                      election.status === "upcoming"
+                        ? "text-blue-500 bg-blue-500/20"
+                        : election.status === "open"
+                        ? "text-emerald-500 bg-emerald-500/20"
+                        : ""
+                    }`}
+                  >
+                    {election.status}
+                  </span>
+                </TableCell>
+                <TableCell>
+                  <span className="overflow-hidden text-ellipsis max-w-[200px] flex truncate break-all">
+                    {election.description} ...
+                  </span>
+                </TableCell>
+                <TableCell className="items-center text-center">
+                  <Popover>
+                    <PopoverTrigger>
+                      {election.candidates.length}
+                    </PopoverTrigger>
+                    <PopoverContent>
+                      {election.candidates.map((candidate) => (
+                        <div
+                          key={candidate.id}
+                          className="flex items-center px-1 space-x-1 rounded-md cursor-pointer hover:bg-bg hover:text-white hover:font-medium"
+                        >
+                          <Link href={`/candidates/${candidate.id}`}>
+                            {candidate.name}
+                          </Link>
+                        </div>
+                      ))}
+                    </PopoverContent>
+                  </Popover>
+                </TableCell>
+                <TableCell className="text-center">0%</TableCell>
+                <TableCell className="flex items-center justify-center h-full gap-1 mt-2 text-xl ">
+                  <MdDelete className="h-full" />
+                  <MdUpdate />
+                  <MdMore className="rotate-180" />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+      {/* <hr className="my-4 border-4 text-bgSoft border-bgSoft" /> */}
+      <Link href="/elections/new">
+        <div className="flex justify-center p-2 mt-4 border rounded-md bg-bgSoft hover:bg-textSoft hover:text-black border-textSoft">
+          <span>Create New Election</span>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default ElectionsPage;
---
+++ data.js
@@ -0,0 +1,102 @@
+export const elections = [
+  {
+    id: 1,
+    title: "2022 General Elections",
+    startDate: "2022-08-09",
+    endDate: "2022-08-09",
+    status: "upcoming",
+    description:
+      "The 2022 General Elections will be held on the 9th of August 2022. The elections will be held in all the 47 counties in Kenya.",
+    candidates: [
+      {
+        id: 1,
+        name: "John Doe",
+        party: "Jubilee Party",
+        manifesto:
+          "I am John Doe and I am running for the position of President of the Republic of Kenya. I am a visionary leader who has the best interests of the people at heart.",
+        votes: [
+          {
+            id: 1,
+            voterId: 1,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "2024 Elections",
+    startDate: "2022-08-09",
+    endDate: "2022-08-09",
+    status: "open",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam libero justo laoreet sit amet cursus sit amet dictum. Lacus laoreet non curabitur gravida arcu ac tortor dignissim convallis. In iaculis nunc sed augue lacus viverra vitae congue. Vulputate ut pharetra sit amet aliquam id diam. Et egestas quis ipsum suspendisse ultrices gravida dictum fusce. Eget dolor morbi non arcu risus quis varius quam quisque. Cursus turpis massa tincidunt dui ut ornare lectus. Enim diam vulputate ut pharetra sit amet aliquam. Id porta nibh venenatis cras sed felis eget velit. Hac habitasse platea dictumst quisque sagittis purus sit amet volutpat. Imperdiet dui accumsan sit amet nulla facilisi. Suscipit adipiscing bibendum est ultricies integer quis. Nisl nunc mi ipsum faucibus vitae. Non pulvinar neque laoreet suspendisse interdum. Maecenas volutpat blandit aliquam etiam erat velit scelerisque in. Lectus quam id leo in.      Rhoncus urna neque viverra justo nec. Tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Amet luctus venenatis lectus magna fringilla urna porttitor rhoncus. Ullamcorper morbi tincidunt ornare massa. Libero justo laoreet sit amet cursus. Nunc lobortis mattis aliquam faucibus purus in massa tempor. Dui ut ornare lectus sit amet est placerat in egestas. Sit amet consectetur adipiscing elit ut aliquam. Quam quisque id diam vel quam elementum pulvinar etiam. Tincidunt arcu non sodales neque sodales ut etiam. Enim praesent elementum facilisis leo vel fringilla est ullamcorper eget. Dictum at tempor commodo ullamcorper. Interdum varius sit amet mattis vulputate enim nulla aliquet. Commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend. Eget arcu dictum varius duis at consectetur lorem. Diam quis enim lobortis scelerisque fermentum dui faucibus in. In tellus integer feugiat scelerisque varius morbi enim nunc. In nisl nisi scelerisque eu ultrices vitae auctor.       Maecenas accumsan lacus vel facilisis volutpat est velit. Egestas integer eget aliquet nibh praesent. Ut pharetra sit amet aliquam id diam maecenas. Posuere morbi leo urna molestie at elementum eu. Consequat semper viverra nam libero justo. Risus viverra adipiscing at in tellus integer. Lectus proin nibh nisl condimentum id venenatis a condimentum. Netus et malesuada fames ac turpis egestas maecenas. Tincidunt eget nullam non nisi est sit. Varius vel pharetra vel turpis nunc eget lorem dolor. Cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris. Pellentesque pulvinar pellentesque habitant morbi tristique senectus. Urna molestie at elementum eu facilisis sed. Nisi porta lorem mollis aliquam ut porttitor leo a",
+    candidates: [
+      {
+        id: 1,
+        name: "John Doe",
+        party: "Jubilee Party",
+        manifesto:
+          "I am John Doe and I am running for the position of President of the Republic of Kenya. I am a visionary leader who has the best interests of the people at heart.",
+        votes: [
+          {
+            id: 1,
+            voterId: 1,
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "Jane Doe",
+        party: "Jubilee Party",
+        manifesto:
+          "I am John Doe and I am running for the position of President of the Republic of Kenya. I am a visionary leader who has the best interests of the people at heart.",
+        votes: [
+          {
+            id: 1,
+            voterId: 1,
+          },
+        ],
+      },
+      {
+        id: 3,
+        name: "Albert Doe",
+        party: "Jubilee Party",
+        manifesto:
+          "I am John Doe and I am running for the position of President of the Republic of Kenya. I am a visionary leader who has the best interests of the people at heart.",
+        votes: [
+          {
+            id: 1,
+            voterId: 1,
+          },
+        ],
+      },
+      {
+        id: 4,
+        name: "Roddy Doe",
+        party: "Jubilee Party",
+        manifesto:
+          "I am John Doe and I am running for the position of President of the Republic of Kenya. I am a visionary leader who has the best interests of the people at heart.",
+        votes: [
+          {
+            id: 1,
+            voterId: 1,
+          },
+        ],
+      },
+      {
+        id: 5,
+        name: "Kimothian Doe",
+        party: "Jubilee Party",
+        manifesto:
+          "I am John Doe and I am running for the position of President of the Republic of Kenya. I am a visionary leader who has the best interests of the people at heart.",
+        votes: [
+          {
+            id: 1,
+            voterId: 1,
+          },
+        ],
+      },
+    ],
+  },
+];
